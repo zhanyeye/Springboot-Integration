b22e50ea20eae3099b5ebc975c399dfd2f225d8d
zhanyeye <zhanyeye@qq.com>
2019-05-24 15:27:53 +0800

    在one端取消many关联，在many端持久化组件添加查询语句/方法

--- src/main/java/com/example/springbootexamples/entity/User.java
+++ src/main/java/com/example/springbootexamples/entity/User.java
@@ -21,8 +21,10 @@ public class User {
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) //禁止User中密码属性序列化
     private String password;
     private int authorityId = 1;
-    @OneToMany(mappedBy = "user")
-    private List<Address> addresses;
+    //当序列化addresses时，user-address的双向依赖导致死循环！
+    //在one端取消many关联，在many端持久化组件添加查询语句/方法
+    //@OneToMany(mappedBy = "user")
+    //private List<Address> addresses;
     @Column(columnDefinition = "timestamp not null default current_timestamp", updatable = false, insertable = false)
     private LocalDateTime insertTime;
     public User(int id) {
